4be7709a313dab587ca1e9793757172fa1f93c28
Aaron Beavers <foamy125@gmail.com>
2023-10-10 17:12:19 -0400

    Shakey speary testy westy

--- services/discord-message-generator/main.py
+++ services/discord-message-generator/main.py
@@ -1,6 +1,7 @@
 """This service creates messages from the model and saves them in mongodb
 """
 from shared.textgenrnn.textgenrnn import textgenrnn
+from shared.mongodb import discord_message_collection
 
 import os
 import random
@@ -10,7 +11,44 @@ import datetime
 from shared.mongodb import generated_message_collection
 import shared.settings as settings
 
+def extract_hashtags_from_string(string):
+    return [hashtag.lower() for hashtag in string.split() if hashtag.startswith("#")]
 
+def remove_hashtag_from_string(string):
+    return " ".join(word for word in string.split() if not word.startswith("#"))
+
+def extract_mentions_from_string(string):
+    return [mention.lower() for mention in string.split() if mention.startswith("@")]
+
+def remove_mention_from_string(string):
+    return " ".join(word for word in string.split() if not word.startswith("@"))
+
+def extract_urls_from_string(string):
+    return [url.lower() for url in string.split() if url.startswith("http")]
+
+def remove_url_from_string(string):
+    return " ".join(word for word in string.split() if not word.startswith("http"))
+
+def encode_sample(message):
+    return {
+        "recipient":message['recipient'],
+        "channel":message['channel'],
+        "channel_name": message['channel_name'],
+        "content":remove_hashtag_from_string(
+            remove_mention_from_string(
+                remove_url_from_string(
+                    message['content']
+                )
+            )
+        )
+    }
+
+def collect_samples(size=10):
+    return discord_message_collection.aggregate([
+        { "$match": { "recipient":settings.DISCORD_CLIENT_USER_ID} },
+        {"$sample":{"size":size}},
+    ], allowDiskUse=True)
+    # Collect all channels first
 
 def generate_batch(temp) -> str:
     # We load this model in to not conflict with anything happening in training.
@@ -30,17 +68,16 @@ def generate_batch(temp) -> str:
             'single_text': settings.TEXTGEN_SINGLE_TEXT
         }
     )
-    first_pass=json.loads(model.generate(
-        progress=False,
-        n=1,
-        prefix='{"recipient":"'+str(settings.DISCORD_CLIENT_USER_ID),
-        # Give it a head start and let it know we mean buisness with JSON
-        temperature=temp,
-        return_as_list=True)[0])
+    prefix_samples=collect_samples(9)
+    prefix="["
+    for sample in prefix_samples:
+        print("accumulating frame prefix",sample)
+        prefix+=json.dumps(encode_sample(sample), separators=(",",":"))+','
+
     return model.generate(
         progress=False,
         n=1,
-        prefix='{"recipient":"'+str(settings.DISCORD_CLIENT_USER_ID)+',"channel":'+str(first_pass['channel'])+',"channel_name":"'+first_pass['channel_name']+'",',
+        prefix=prefix,
         # Give it a head start and let it know we mean buisness with JSON
         temperature=temp,
         return_as_list=True)[0]
@@ -54,13 +91,13 @@ while True:
         )
         started=datetime.datetime.utcnow()
         sample=generate_batch(temp)
+        print("sample generated",sample)
         finished=datetime.datetime.utcnow()
     except Exception as e:
         print("failed")
         print(e)
         continue
     try:
-
         print("batch complete, saving samples")
         print("temp",temp)
         sample_data=json.loads(sample)
